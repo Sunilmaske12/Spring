4713208ed8ed92b29b8705ef8818c7c2b8e70507
INTEGRAL COMPUTER <INTEGRAL COMPUTER@INTEGRALCOMPUTE>
2023-04-19 08:10:59 +0530

    Collection using constructor injection

--- Spring2/src/main/java/com/springCore/ConstructorInjection/App.java
+++ Spring2/src/main/java/com/springCore/ConstructorInjection/App.java
@@ -1,10 +1,15 @@
 package com.springCore.ConstructorInjection;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/springCore/ConstructorInjection/student.xml");
+		Student student=(Student) context.getBean("student");
+		student.display();
+		((AbstractApplicationContext) context).close();
 	}
-
 }
--- Spring2/src/main/java/com/springCore/ConstructorInjection/Student.java
+++ Spring2/src/main/java/com/springCore/ConstructorInjection/Student.java
@@ -1,5 +1,35 @@
 package com.springCore.ConstructorInjection;
 
-public class Student {
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+public class Student {
+	int id;
+	String name;
+	int marks;
+	List<String> subject;
+	Map<String, String> relative;
+	Set<String> friends;
+	public Student(int id, String name, int marks,List<String> subject,Map<String, String> relative, Set<String> friends ) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+		this.subject=subject;
+		this.relative=relative;
+		this.friends=friends;
+	}
+	
+	public void display() {
+		System.out.println("id "+id);
+		System.out.println("name "+name);
+		System.out.println("marks "+marks); 
+		Iterator<String> it=subject.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+	
 }
