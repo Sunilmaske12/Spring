8def283a26bbe830e5888f3ed5b0906c334532ff
INTEGRAL COMPUTER <INTEGRAL COMPUTER@INTEGRALCOMPUTE>
2023-06-18 13:31:13 +0530

    Registration data saving in database using springORM with springMVC

--- SpringMVC/src/main/java/com/spring/controller/SpringMVC_SpringORM.java
+++ SpringMVC/src/main/java/com/spring/controller/SpringMVC_SpringORM.java
@@ -2,6 +2,9 @@ package com.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,6 +12,7 @@ import com.spring.entity.User;
 import com.spring.service.UserService;
 
 @Component
+@Controller
 public class SpringMVC_SpringORM {
 	
 	@Autowired
@@ -19,9 +23,13 @@ public class SpringMVC_SpringORM {
 		return "registrationDB";
 	}
 	
-	@RequestMapping(name="/registrationDB", method=RequestMethod.POST)
-	public String registerUserDB(User user) {
+	@RequestMapping(path="/registrationDB", method=RequestMethod.POST)
+	public String registerUserDB(@ModelAttribute("user") User user, Model model) {
 		this.userService.createUser(user);
+		//here given annotation automatically set the values, if we use jstl then we dont need to add in model also, it can directly fetch as user.name ...
+		model.addAttribute("name",user.getName());
+		model.addAttribute("email",user.getEmail());
+		model.addAttribute("age",user.getAge());
 		return "registrationDBDetail";
 	}
 }
--- SpringMVC/src/main/java/com/spring/dao/UserDao.java
+++ SpringMVC/src/main/java/com/spring/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.spring.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,7 @@ public class UserDao {
 	@Autowired
 	HibernateTemplate hibernateTemplate;
 	
+	@Transactional
 	public int saveUser(User user) {
 		int result = (int)this.hibernateTemplate.save(user);
 		return result;
